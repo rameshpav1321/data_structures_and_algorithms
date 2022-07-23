ed6b9c2b4b06456255a7c1e4bb1b1a83bd913193
Ramesh Pavan Pothamsetty <rameshpavan.p@outlook.com>
2022-07-23 12:48:56 -0400

    prime factors

--- math/6.prime_factors.py
+++ math/6.prime_factors.py
@@ -1,28 +1,34 @@
 import math
 
+# def is_prime(x):
+#     if x == 1:
+#         return False
+#     if x == 2 or x == 3:
+#         return True
+#     for j in range(2, int(math.sqrt(x)+1)):
+#         if(x % j == 0):
+#             return False
+#     return True
+# if __name__ == "__main__":
+#     n = 450
+#     i = 2
+#     while n != 1:
+#         if is_prime(i):
+#             while n % i == 0:
+#                 print(i)
+#                 n //= i
+#         i += 1
 
-def is_prime(x):
-    print("func called")
-    if x == 1:
-        return False
-    if x == 2 or x == 3:
-        print("hi")
-        return True
-    for j in range(2, int(math.sqrt(x)+1)):
-        if(x % j == 0):
-            return False
-    return True
+
+def prime_factors(n):
+    if n <= 1:
+        return
+    for i in range(2, int(math.sqrt(n)+1)):
+        while n % i == 0:
+            print(i)
+            n //= i
+    if n > 1:
+        print(n)
 
 
-if __name__ == "__main__":
-    n = 450
-    print("vikas")
-    i = 2
-    while n != 1:
-        print("loop started")
-        print(i)
-        if(is_prime(i)):
-            if(n % i == 0):
-                print(i)
-                n //= i
-                i += 1
+prime_factors(1)
