8f2e16a3117428aca2d45848641f9198958f5d36
Ramesh Pavan Pothamsetty <rameshpavan.p@outlook.com>
2023-01-25 11:51:36 -0500

    llist

--- linked_list/7.reverse_llist.py
+++ linked_list/7.reverse_llist.py
@@ -10,28 +10,4 @@ class Solution:
             curr=temp
         return prev
 
-# --------------------------------
-class Solution:
-    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if not head:
-            return
-        stack=[]
-        curr=head
-        while curr.next!=None:
-            stack.append(curr)
-            curr=curr.next
-        new_head=curr
-        for i in range(len(stack)):
-            curr.next=stack.pop()
-            curr=curr.next
-        curr.next=None
-        return new_head
-# ------------------------------------
-class Solution:
-    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if head==None or head.next==None:
-            return head
-        rev=self.reverseList(head.next)
-        head.next.next=head
-        head.next=None
-        return rev
+
